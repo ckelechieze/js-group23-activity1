c01902f8c1527c2cc5788098f798cc9e0eb61423
Cornelius Kelechi Eze <ckelechieze@gmail.com>
2026-08-09 21:19:38 +0100

    initial commit

---
+++ script.js
@@ -0,0 +1,14 @@
+// Welcome program that greets the user by name and tells them the year they were born based on their age input.
+
+//let userName = prompt("What is your name?");
+//let age = prompt("How old are you?");
+
+//let birthYear = 2026 - age;
+//let favoriteHobby = prompt("What is your favorite hobby?");
+
+//alert(`Hello, ${userName}! You were born in ${birthYear}, and your favorite hobby is ${favoriteHobby}.`);
+
+
+console.log("girl & " + "boy"); 
+
+console.log("1 " + "2 " + "3 ");
